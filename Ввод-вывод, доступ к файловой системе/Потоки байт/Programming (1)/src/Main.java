1b827976c652275b438e970e8aebf1c2746314f8
1azyfox <timyr.xas@gmail.com>
2021-07-30 23:47:52 +0300

    новые решения

--- Ввод-вывод, доступ к файловой системе/Потоки байт/Programming (1)/src/Main.java
+++ Ввод-вывод, доступ к файловой системе/Потоки байт/Programming (1)/src/Main.java
@@ -1,2 +1,32 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        byte[] arr = {65, 13, 10, 10, 13};
+        ByteArrayInputStream inputStream0 = new ByteArrayInputStream(arr);
+        System.setIn(inputStream0);
+        try (InputStream inputStream = System.in) {
+            byte[] array = new byte[1];
+            int count = inputStream.read(array);
+            List<Byte> arrayList = new ArrayList();
+            while (count > 0) {
+                for (byte b : array) {
+                    arrayList.add(b);
+                }
+                count = inputStream.read(array);
+            }
+            for (int i = 0; i < arrayList.size() - 1; i++) {
+                if (arrayList.get(i).equals((byte) 13) && arrayList.get(i + 1).equals((byte) 10)) {
+                    arrayList.remove(i);
+                }
+            }
+            for (Object o : arrayList) {
+                System.out.write((byte) o);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Ввод-вывод, доступ к файловой системе/Потоки байт/Programming/src/Main.java
+++ Ввод-вывод, доступ к файловой системе/Потоки байт/Programming/src/Main.java
@@ -1,3 +1,47 @@
-public static int checkSumOfStream(InputStream inputStream) throws IOException {
-    // your implementation here
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        final byte[] data = {0x33, 0x45, 0x01};
+        final byte[] data2 = {};
+
+        try (InputStream inputStream = new ByteArrayInputStream(data)) {
+            System.out.println(checkSumOfStream(inputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (InputStream inputStream2 = new ByteArrayInputStream(data2)) {
+            System.out.println(checkSumOfStream(inputStream2));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        int result = 0;
+        for(int i = inputStream.read(); i != -1; i = inputStream.read()) {
+            int c = Integer.rotateLeft(result,1);
+            result = c ^ i;
+        }
+        return result;
+//        if (inputStream.available() == 0) {
+//            return 0;
+//        }
+//        int a=inputStream.available();
+//        int[] data = new int[a];
+//        for (int j =0; j < a; j ++) {
+//            data[j] = inputStream.read();
+//        }
+//        int result = 0;
+//        for (int i = 0; i < data.length - 1; i++) {
+//            int c = ((Integer.rotateLeft(data[i+1],1)));
+//            int b = data[i];
+//            result = c ^ b;
+//        }
+//        return result;
+    }
 }
