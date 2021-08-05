273d46f7ab7a1780e0e8d7d5a152cfeff83dde68
1azyfox <timyr.xas@gmail.com>
2021-08-05 23:56:57 +0300

    новые решения

--- Generics. Collections. Streams/Коллекции/Programming (1)/src/Main.java
+++ Generics. Collections. Streams/Коллекции/Programming (1)/src/Main.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+            for (int i = 0; scanner.hasNext(); i++) {
+                if (i==0 || i % 2 == 0) {
+                    scanner.nextInt();
+                } else {
+                    arrayDeque.add(scanner.nextInt());
+                }
+            }
+        scanner.close();
+        while (!arrayDeque.isEmpty()) {
+            System.out.print(arrayDeque.removeLast() + " ");
+        }
+    }
 }
